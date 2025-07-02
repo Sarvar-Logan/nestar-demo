4cea27b23ce26e8186fb2008d25ac422e46a6979
Sarvar-Logan <sarvartojeboev@gmail.com>
2025-07-03 00:38:04 +0900

    feat: organize and test pages routing system

---
+++ pages/community/index.tsx
@@ -0,0 +1,11 @@
+const Community = () => {
+  console.log("COMMUNITY COMPONENT - PAGES ROUTER")
+  return (
+    <div>
+      COMMUNITY LIST{""}
+      <button onClick={() => alert("Hello MIT")} style={{margin: "15px"}}>Press me</button>
+    </div>
+  )
+};
+
+export default Community;
