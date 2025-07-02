8736a217d9a04381087732312ba9101a26b56b2a
Sarvar-Logan <sarvartojeboev@gmail.com>
2025-07-03 01:42:51 +0900

    feat: mui customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from "next/app"; // `AppProps`ni to'g'ri import qilish
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { light } from "../scss/MaterialTheme";
+import { useState } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps) { // Sintaksisni to'g'irlash
+  // @ts-ignore: TypeScript uchun, agar xato bo'lsa
+  const [theme, setTheme] = useState(() => createTheme(light)); // To'g'ri `useState` ishlatish
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
