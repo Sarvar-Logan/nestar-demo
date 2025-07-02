258d2a4c3b903b5bbf619616c111abccac89aa9a
Sarvar-Logan <sarvartojeboev@gmail.com>
2025-07-03 01:55:38 +0900

    feat: integration header footer via layout

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,11 +1,18 @@
-const Community = () => {
-  console.log("COMMUNITY COMPONENT - PAGES ROUTER")
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
+import { useState } from "react";
+
+const CommunityList: NextPage = () => {
+  //console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+  const [title, setTitle] = useState<string>("hello");
   return (
     <div>
-      COMMUNITY LIST{""}
-      <button onClick={() => alert("Hello MIT")} style={{margin: "15px"}}>Press me</button>
+      COMMUNITY
+      <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
+        Press Me
+      </button>
     </div>
-  )
+  );
 };
 
-export default Community;
+export default withLayoutBasic(CommunityList);
