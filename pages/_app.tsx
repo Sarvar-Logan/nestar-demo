a26a6abeb30ac161c2b242c4351c2411570a1ccf
Sarvar-Logan <sarvartojeboev@gmail.com>
2025-07-10 17:38:05 +0900

    feat: apollo client connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 import "../scss/app.scss";
 import "../scss/pc/main.scss";
 import "../scss/mobile/main.scss"
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 
 
 
@@ -14,9 +16,11 @@ export default function App({ Component, pageProps }: AppProps) { // Sintaksisni
   const [theme, setTheme] = useState(() => createTheme(light)); // To'g'ri `useState` ishlatish
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
